f4d189cb6b8a359d61ceaa65c7cc0e69267b779e
Vasu Grover <notcoderguy@gmail.com>
2024-12-19 13:31:48 +0530

    feat: Add local api usage for development

--- api/main.py
+++ api/main.py
@@ -21,7 +21,8 @@ origins = [
     "https://notcoderguy.com",
     "http://notcoderguy.com",
     "https://spotifyx.notcoderguy.com",
-    "http://spotifyx.notcoderguy.com"
+    "http://spotifyx.notcoderguy.com",
+    "http://localhost:8000"
 ]
 
 app.add_middleware(
